418f1be69f18712de14bd69a34c53e31a068bd34
Tom Lynch <tom@amristar.com>
2016-08-30 23:24:40 +0800

    add GDAL dependency

--- setup.py
+++ setup.py
@@ -3,15 +3,17 @@ from frisson.version import Version
 
 
 setuptools.setup(name='frisson',
-                 version=Version('0.0.1').number,
-                 description='Frisson Georeferencing Toolkit',
-                 long_description=open('README.md').read().strip(),
-                 author='Tom Lynch',
-                 author_email='',
-                 url='http://github.com/attentive/frisson',
-                 py_modules=['frisson'],
-                 install_requires=[],
-                 license='MIT License',
-                 zip_safe=False,
-                 keywords='georeferencing spatial imagery package',
-                 classifiers=['Packages', 'Spatial'])
+        version=Version('0.0.1').number,
+        description='Frisson Georeferencing Toolkit',
+        long_description=open('README.md').read().strip(),
+        author='Tom Lynch',
+        author_email='',
+        url='http://github.com/attentive/frisson',
+        py_modules=['frisson'],
+        install_requires=[
+            'GDAL',
+            ],
+        license='MIT License',
+        zip_safe=False,
+        keywords='georeferencing spatial imagery package',
+        classifiers=['Packages', 'Spatial'])
