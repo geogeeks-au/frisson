10fb16148811fd4bd81011e527795df5effdda01
Cameron Poole <Cameron.j.poole@gmail.com>
2016-09-15 22:52:25 +0800

    Added shape file index test function

--- frisson/frisson.py
+++ frisson/frisson.py
@@ -1,7 +1,8 @@
-#from osgeo import gdal
-#from osgeo import ogr
+# from osgeo import gdal
+# from osgeo import ogr
 from subprocess import call
 from os import environ, path
+
 
 # https://docs.python.org/2/library/subprocess.html
 # subprocess.call(args, *, stdin=None, stdout=None, stderr=None, shell=False)
@@ -9,16 +10,17 @@ from os import environ, path
 # $GDAL_PATH/gdal_translate $INPUTFILE -outsize $DEST_WIDTH $DESTHEIGHT \ 
 # -co COMPRESS=DEFLATE -co PHOTOMETRIC=RGB -co PROFILE=BASELINE $OUTPUT_TIF
 
-def convert_to_tiff(filename, output_filename): 
-    args = [path.join(environ["GDAL_HOME"], "gdal_translate"), 
-          filename, 
-          "-outsize", 
-          "1000", "1000", 
-          "-co", "COMPRESS=DEFLATE",
-          "-co", "PHOTOMETRIC=RGB", 
-          "-co", "PROFILE=BASELINE",
-          output_filename]
+def convert_to_tiff(filename, output_filename):
+    args = [path.join(environ["GDAL_HOME"], "gdal_translate"),
+            filename,
+            "-outsize",
+            "1000", "1000",
+            "-co", "COMPRESS=DEFLATE",
+            "-co", "PHOTOMETRIC=RGB",
+            "-co", "PROFILE=BASELINE",
+            output_filename]
     return call(args)
+
 
 def add_overviews(filename):
     args = [path.join(environ["GDAL_HOME"], "gdaladdo"),
@@ -33,7 +35,8 @@ def add_overviews(filename):
             "64"]
     return call(args)
 
-def create_mask_vectors(output_filename, input_folder, extension="*.tif"):
+
+def create_mask_vectors(output_filename, input_filename):
     """
     $GDAL_PATH / gdaltindex - write_absolute_path $OUTPUT_INDEX_SHAPEFILE $INPUT_FOLDER / *.$EXT
     :return:
@@ -41,21 +44,22 @@ def create_mask_vectors(output_filename, input_folder, extension="*.tif"):
     args = [path.join(environ["GDAL_HOME"], "gdaltindex"),
             "-write_absolute_path",
             output_filename,
-            input_folder,
-            extension]
+            input_filename
+            ]
     return call(args)
-
 
 
 def add_mask(filename, mask_vectors):
     pass
 
-def virtual_georeference(filename, control_points): 
+
+def virtual_georeference(filename, control_points):
     pass
 
-def georeference(filename, control_points, opts): 
+
+def georeference(filename, control_points, opts):
     pass
 
-def get_map_bbbox(filename): 
-    pass
 
+def get_map_bbbox(filename):
+    pass
--- tests/unit/test_frisson.py
+++ tests/unit/test_frisson.py
@@ -1,14 +1,18 @@
 from tests import *
 from os import remove, path
 
-from frisson.frisson import convert_to_tiff, add_overviews 
+from frisson.frisson import (
+    convert_to_tiff,
+    add_overviews,
+    create_mask_vectors
+)
+
 
 class TestPrintPoint(unittest.TestCase):
-
     def test_input_exists(self):
         self.assertTrue(path.isfile("tests/cons 1636 item 3706.tif"), "Test File doesn't exist")
 
-    #@pytest.mark.xfail
+    # @pytest.mark.xfail
     def test_convert_to_tiff(self):
         self.assertEquals(convert_to_tiff("tests/cons 1636 item 3706.tif",
                                           "tests/test_output.tif"),
@@ -23,9 +27,12 @@ class TestPrintPoint(unittest.TestCase):
         self.assertEquals(add_overviews("tests/test_output.tif"), 0,
                           "Couldn't create overview")
 
+    def test_shapefile_index(self):
+        # TODO: Not 100% on this one
+        self.assertEquals(create_mask_vectors("tests/test_output.shp", "tests/test_output.tif"), 0,
+                          "Couldn't create shapefile index")
 
     def tearDown(self):
         if path.isfile("tests/test_output.tif"):
             pass
-            #remove("tests/test_output.tif")
-
+            # remove("tests/test_output.tif")
