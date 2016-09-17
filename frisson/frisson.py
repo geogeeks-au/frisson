8c5a5f75fb9e7c5a4385a8586df3e77d0ad54a54
Cameron Poole <Cameron.j.poole@gmail.com>
2016-09-17 22:05:49 +0800

    Helper function for gcp list to string list

--- frisson/frisson.py
+++ frisson/frisson.py
@@ -49,8 +49,45 @@ def create_mask_vectors(output_filename, input_filename):
     return call(args)
 
 
-def add_mask(filename, mask_vectors):
-    pass
+def _points_to_string(gcp_points):
+    """
+    Converts a list of GCP points to a gdal_translate gcp argument string
+    :param gcp_points: A list of GCP tuples
+    :return: A list of formatted strings
+    """
+    gcp_s = []
+    for gcp in gcp_points:
+        assert type(gcp) in [list, tuple]
+        assert len(gcp) == 4
+        gcp_s.append(["-gcp"])
+        coords = []
+        for coord in gcp:
+            if type(coord) == float:
+                coord = str(coord)
+            coords.append(coord)
+
+        gcp_s[-1].append(" ".join(coords))
+    return gcp_s
+
+
+def add_mask(input_filename, output_filename, mask_vectors):
+    """
+    gdal_translate -of GTiff -gcp 1989.12 2999.84 1.28952e+07 -3.75616e+06 -gcp 4083.48 3103.33 1.28981e+07 -3.75636e+06 -gcp 4108.23 2151.34 1.28982e+07 -3.755e+06 "/Users/Drogon/Repos/frisson/tests/cons 1636 item 3706.tif" "/var/folders/nd/k10412g92s1bk7rll01d59y40000gn/T/cons 1636 item 3706.tif"
+    gdalwarp -r near -order 1 -co COMPRESS=NONE  "/var/folders/nd/k10412g92s1bk7rll01d59y40000gn/T/cons 1636 item 3706.tif" "/Users/Drogon/Repos/frisson/tests/cons 1636 item 3706_testref.tif"
+    $GDAL_PATH/gdal_translate -a_srs '+init=epsg:4326' -of VRT \
+$CONVERTED_TIF $VIRTUAL_WARPED.vrt $MAGIC_GCP_STRING
+    mask_vectors should be a list of tuples (x,y,long,lat)
+    """
+    args = [path.join(environ["GDAL_HOME"], "gdal_translate"),
+            "-a_srs",
+            "+init=epsg:4326",
+            "-of",
+            "VRT"] + [x for y in _points_to_string2(mask_vectors) for x in y] + [
+            input_filename,
+            output_filename,
+            ]
+    print(args)
+    return call(args)
 
 
 def virtual_georeference(filename, control_points):
--- tests/unit/test_frisson.py
+++ tests/unit/test_frisson.py
@@ -4,11 +4,28 @@ from os import remove, path
 from frisson.frisson import (
     convert_to_tiff,
     add_overviews,
-    create_mask_vectors
+    create_mask_vectors,
+    add_mask,
+    _points_to_string
 )
 
 
 class TestPrintPoint(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.gcp_points = [
+            [12895157.1042, -3756156.6509, 1989.1230,  -2999.8393],
+            [12898106.0028, -3756364.7068, 4083.4782, -3103.3251],
+            [12898161.6258, -3754995.9836, 4108.2269, -2151.3432]
+        ]
+        cls.gcp_string = [
+            ["-gcp", "12895157.1042 -3756156.6509 "
+                     "1989.123 -2999.8393"],
+            ["-gcp", "12898106.0028 -3756364.7068 "
+                     "4083.4782 -3103.3251"],
+            ["-gcp", "12898161.6258 -3754995.9836 "
+                     "4108.2269 -2151.3432"]
+        ]
     def test_input_exists(self):
         self.assertTrue(path.isfile("tests/cons 1636 item 3706.tif"), "Test File doesn't exist")
 
@@ -32,6 +49,17 @@ class TestPrintPoint(unittest.TestCase):
         self.assertEquals(create_mask_vectors("tests/test_output.shp", "tests/test_output.tif"), 0,
                           "Couldn't create shapefile index")
 
+    def test_add_mask(self):
+        self.assertEquals(add_mask("tests/test_output.tif",
+                                   "tests/test_output.vrt",
+                                   self.gcp_points), 0,
+                          "Couldn't add mask")
+
+    def test_gcp_string(self):
+        self.assertEquals(_points_to_string(self.gcp_points),
+                          self.gcp_string
+                          )
+
     @classmethod
     def tearDownClass(self):
         test_files = [
@@ -42,5 +70,6 @@ class TestPrintPoint(unittest.TestCase):
                       ]
         for tf in test_files:
             if path.isfile(tf):
-                remove(tf)
+                pass
+                #remove(tf)
 
